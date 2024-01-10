2759712b489310529667dd17eead9e551e4f25a0
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2024-01-10 05:49:07 +0530

    jetpack compose and udemy project

--- google-tutorials/JetPackCompose/app/src/main/java/com/lakshaykamat/jetpackcompose/MainActivity.kt
+++ google-tutorials/JetPackCompose/app/src/main/java/com/lakshaykamat/jetpackcompose/MainActivity.kt
@@ -3,22 +3,29 @@ package com.lakshaykamat.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.lakshaykamat.jetpackcompose.ui.theme.JetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,35 +38,64 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
+                    Column(
+                        Modifier
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState())
+                    ) {
                         val image = painterResource(id = R.drawable.bg_compose_background)
-                        val imageDescription = "Background image of jetpack compose"
-                        Image(painter = image, contentDescription = imageDescription)
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize()
-                                .padding(16.dp)
+                        ArticleImage(imagePainter = image, imageDescription = R.string.image_description)
+                        ArticleText(
+                            headingLineID = R.string.title_jetpack_compose_tutorial,
+                            shortDescription = R.string.compose_short_desc,
+                            longDescription = R.string.compose_long_desc
+                        )
+                        Row(
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp, vertical = 32.dp),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween
                         ) {
-                            Text(
-                                text = stringResource(id = R.string.title_jetpack_compose_tutorial),
-                                modifier = Modifier.padding(top = 16.dp, bottom = 32.dp),
-                                style = MaterialTheme.typography.headlineLarge
-                            )
-                            Text(
-                                text = stringResource(id = R.string.compose_short_desc),
-                                style = MaterialTheme.typography.bodyLarge,
-                                modifier = Modifier.padding(bottom = 16.dp)
-                            )
-                            Text(
-                                text = stringResource(id = R.string.compose_long_desc),
-                                style = MaterialTheme.typography.bodyLarge
-                            )
+                            OutlinedButton(
+                                onClick = { /*TODO*/ },
+                            ) {
+                                Text(text = "Previous")
+                            }
+                            Button(onClick = { /*TODO*/ }) {
+                                Text(text = "Next")
+                            }
                         }
-
                     }
-
                 }
             }
         }
+    }
+}
+
+@Composable
+fun ArticleImage(imagePainter: Painter, imageDescription:Int){
+    Image(painter = imagePainter, contentDescription = stringResource(id = imageDescription))
+}
+@Composable
+fun ArticleText(@StringRes headingLineID: Int, shortDescription: Int, longDescription: Int) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+    ) {
+        Text(
+            text = stringResource(id = headingLineID),
+            modifier = Modifier.padding(top = 16.dp, bottom = 32.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
+        Text(
+            text = stringResource(id = shortDescription),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = stringResource(id = longDescription),
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
 }
--- udemy-projects/ShoppingListApp/app/src/main/java/com/lakshaykamat/shoppinglistapp/ShoppingList.kt
+++ udemy-projects/ShoppingListApp/app/src/main/java/com/lakshaykamat/shoppinglistapp/ShoppingList.kt
@@ -1,9 +1,8 @@
 package com.lakshaykamat.shoppinglistapp
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,20 +11,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -37,7 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
@@ -65,25 +66,35 @@ fun ShoppingListApp() {
         Spacer(modifier = Modifier.height(32.dp))
         Text(
             modifier = Modifier.align(Alignment.CenterHorizontally),
-            text = "Shopping List",
+            text = stringResource(R.string.app_name),
             style = MaterialTheme.typography.headlineLarge
         )
         Spacer(modifier = Modifier.height(32.dp))
 
         //Showing all list of items
-        ListShoppingItems(shoppingItems = shoppingItems)
+        LazyColumn(modifier = Modifier.fillMaxHeight(.88f)) {
+            items(shoppingItems) { item ->
+                if (item.isEditing) {
+                    ShoppingItemEditor(shoppingItem = item, onEdit = { editName, editQty ->
+                        shoppingItems = shoppingItems.map { it.copy(isEditing = false) }
+                        val editedItem = shoppingItems.find { it.id == item.id }
+                        editedItem?.let {
+                            it.name = editName
+                            it.quantity = editQty
+                        }
+                    })
+                } else {
+                    ItemCardUI(item = item, onEditPress = {
+                        shoppingItems = shoppingItems.map { it.copy(isEditing = item.id == it.id) }
+                    }, {})
+                }
+
+            }
+        }
 
 
         //Button for adding new items
-        Button(
-            onClick = { showDialog = true },
-            modifier = Modifier
-                .fillMaxWidth(0.7f)
-                .padding(16.dp)
-                .align(Alignment.CenterHorizontally)
-        ) {
-            Text(text = "Add Item")
-        }
+        AddItemButton(onPress = { showDialog = true })
 
         //Alert dialog
         if (showDialog) {
@@ -120,40 +131,123 @@ fun ShoppingListApp() {
                 },
                 title = { Text(text = "New Shopping Item") },
                 text = {
-                    //Shopping Item Form
-                    Column {
-                        //Name Input Box
-                        OutlinedTextField(value = itemNameInput,
-                            onValueChange = { itemNameInput = it },
-                            label = { Text(text = "Name") })
-
-                        //Quantity Input Box
-                        OutlinedTextField(value = itemQuantityInput,
-                            onValueChange = { itemQuantityInput = it },
-                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                            label = { Text(text = "Quantity") })
-                    }
-                })
+                    //Shopping Item Editor
+                    CreateShoppingItem(
+                        name = itemNameInput,
+                        quantity = itemQuantityInput,
+                        onNameChange = { newName -> itemNameInput = newName },
+                        onQuantityChange = { newQuantity -> itemQuantityInput = newQuantity },
+                    )
+                }
+            )
         }
     }
 }
 
 @Composable
-fun ListShoppingItems(shoppingItems: List<ShoppingItem>) {
-    LazyColumn(modifier = Modifier.fillMaxHeight(0.9f)) {
-        items(shoppingItems) { item ->
-            ItemCardUI(item = item)
+fun ShoppingItemEditor(shoppingItem: ShoppingItem, onEdit: (String, Int) -> Unit) {
+    var editingName by remember { mutableStateOf(shoppingItem.name) }
+    var editingQuantity by remember { mutableStateOf(shoppingItem.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(shoppingItem.isEditing) }
+
+    Column {
+        //Name Input Box
+        OutlinedTextField(value = editingName,
+            onValueChange = { editingName = it },
+            label = { Text(text = "Name") })
+
+        //Quantity Input Box
+        OutlinedTextField(value = editingQuantity,
+            onValueChange = { editingQuantity = it },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+            label = { Text(text = "Quantity") })
+
+        Button(onClick = {
+            isEditing = false
+            onEdit(editingName, editingQuantity.toIntOrNull() ?: 1)
+        }) {
+            Text(text = "Save")
         }
     }
 }
 
 @Composable
-fun ItemCardUI(item: ShoppingItem) {
+fun CreateShoppingItem(
+    name: String, quantity: String, onNameChange: (String) -> Unit,
+    onQuantityChange: (String) -> Unit
+) {
+    Column {
+        //Name Input Box
+        OutlinedTextField(value = name,
+            onValueChange = { onNameChange(it) },
+            label = { Text(text = "Name") })
+
+        //Quantity Input Box
+        OutlinedTextField(value = quantity,
+            onValueChange = { onQuantityChange(it) },
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+            label = { Text(text = "Quantity") })
+    }
+}
+
+@Composable
+fun AddItemButton(onPress: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(vertical = 4.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        FloatingActionButton(
+            onClick = onPress,
+            modifier = Modifier
+                .size(80.dp)
+                .padding(16.dp),
+            containerColor = MaterialTheme.colorScheme.primary,
+            shape = CircleShape,
+        ) {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add",
+                tint = Color.White
+            )
+        }
+    }
+}
+
+//@Composable
+//fun ListShoppingItems(shoppingItems: MutableList<ShoppingItem>) {
+//    LazyColumn(modifier = Modifier.fillMaxHeight(.88f)) {
+//        items(shoppingItems) { item ->
+//            if (item.isEditing) {
+//                ShoppingItemEditor(shoppingItem = item, onEdit = { editName, editQty ->
+//                    s = shoppingItems.map { it.copy(isEditing = false) }
+//                    val editedItem = s.find { it.id == item.id }
+//                    editedItem?.let {
+//                        it.name = editName
+//                        it.quantity = editQty
+//                    }
+//                })
+//            } else {
+//                ItemCardUI(item = item, onEditPress = {
+//                    shoppingItems = shoppingItems.map { it.copy(isEditing = item.id == it.id) }
+//                }, {})
+//            }
+//
+//        }
+//    }
+//}
+
+@Composable
+fun ItemCardUI(
+    item: ShoppingItem,
+    onEditPress: () -> Unit,
+    onDeletePress: () -> Unit
+) {
     Card(
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth(),
-        //elevation = 16.dp,
         shape = RoundedCornerShape(10.dp), border = BorderStroke(1.dp, Color.LightGray)
     )
     {
@@ -178,14 +272,12 @@ fun ItemCardUI(item: ShoppingItem) {
 
             Column {
                 Row {
-                    IconButton(
-                        onClick = { /*TODO*/ },
-                        content = { Icon(Icons.Default.Edit, contentDescription = "Edit item") })
-                    IconButton(
-                        onClick = { /*TODO*/ },
-                        content = {
-                            Icon(Icons.Default.Delete, contentDescription = "Delete item")
-                        })
+                    IconButton(onClick = onEditPress) {
+                        Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit item")
+                    }
+                    IconButton(onClick = onDeletePress) {
+                        Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete item")
+                    }
                 }
             }
         }
